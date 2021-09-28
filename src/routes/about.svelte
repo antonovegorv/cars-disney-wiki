e40b5cd6f9856ef9acc984c484a0bb6c2b5861ca
Egor Antonov <antonovegorv@mail.ru>
2021-09-28 16:30:45 +0300

    Added about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,42 @@
+<section class="mb-8">
+    <h1 class="text-4xl font-light font-roboto">Краткая Информация</h1>
+    <p class="mt-2 text-xl font-montserrat">
+        На этом сайте вы можете найти информацию о мультфильме «Тачки» и о его персонажах.
+        Сайт не несет коммерческой цели. Создан исключительно в учебных целях.
+    </p>
+</section>
+
+<section class="my-8">
+    <h2 class="text-4xl font-light font-roboto">Работа с API</h2>
+    <p class="mt-2 text-xl font-montserrat">
+        Для того, чтобы использовать API вам не нужно создавать никаких ключей. Вы сразу можете попробовать отправить запрос, и посмотреть, что он вернет.
+    </p>
+    
+    <h3 class="mt-4 text-xl font-roboto">API Endpoints</h3>
+    <ul>
+        <li class="font-medium">
+            GET: <a href="/api/cars" class="font-mono bg-gray-200" target="_blank">cars-disney-wiki.herokuapp.com/api/cars</a> – выводит список всех персонажей
+        </li>
+        <li class="font-medium">
+            GET: <a href="/api/cars/0" class="font-mono bg-gray-200" target="_blank">cars-disney-wiki.herokuapp.com/api/cars/[id]</a> – выводит информацию о персонаже с заданным [id]
+        </li>
+    </ul>
+</section>
+
+<section class="my-8">
+    <h2 class="text-4xl font-roboto">TODO:</h2>
+    <ul class="list-decimal">
+        <li>
+            <p class="font-raleway text-xl">Увеличить количество персонажей</p>
+        </li>
+        <li>
+            <p class="font-raleway text-xl">Реализовать БД на server-side</p>
+        </li>
+        <li>
+            <p class="font-raleway text-xl">Реализовать страницы с ошибками</p>
+        </li>
+        <li>
+            <p class="font-raleway text-xl">Доработать API</p>
+        </li>
+    </ul>
+</section>
