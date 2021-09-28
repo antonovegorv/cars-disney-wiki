32f3b96ea5d54eceaea9202a0645335084deca8d
Egor Antonov <antonovegorv@mail.ru>
2021-09-28 15:56:37 +0300

    Added carousel to the character page

--- src/routes/characters/[id].svelte
+++ src/routes/characters/[id].svelte
@@ -11,6 +11,17 @@
 </script>
 
 <script>
+	import { onMount } from 'svelte';
+	import EmblaCarousel from 'embla-carousel';
+
+	onMount(() => {
+		const emblaNode = document.querySelector('.embla');
+		const options = { loop: true };
+
+		embla = EmblaCarousel(emblaNode, options);
+	});
+
+	export let embla;
 	export let character;
 </script>
 
@@ -21,7 +32,25 @@
 </section>
 
 <section class="py-8">
-	<h1 class="text-4xl font-light font-roboto">{character.name}</h1>
-	<p class="mt-2 font-montserrat">{character.description[0]}</p>
-	<p class="mt-2 font-montserrat">{character.description[1]}</p>
+	<h1 class="text-4xl font-medium font-roboto">{character.name}</h1>
+	<p class="mt-2 text-xl font-montserrat">{character.description[0]}</p>
+	<p class="mt-2 text-xl font-montserrat">{character.description[1]}</p>
+</section>
+
+<section class="py-8">
+	<h2 class="text-4xl font-medium font-roboto">Галерея</h2>
+	<div class="mt-4 embla">
+		<div class="embla__container">
+			{#each character.images.slides as slide}
+				<div class="embla__slide">
+					<img src="{slide}.webp" alt="{slide}" class="w-11/12 md:w-9/12 mx-auto rounded-lg">
+				</div>
+			{/each}
+		</div>
+	
+		<div class="flex justify-center mt-4">
+			<button class="px-4 py-2 mr-3 rounded-full border-2 border-gray-800" on:click="{embla.scrollPrev()}">Назад</button>
+			<button class="px-4 py-2 rounded-full border-2 border-gray-800" on:click="{embla.scrollNext()}">Вперед</button>
+		</div>
+	</div>
 </section>
