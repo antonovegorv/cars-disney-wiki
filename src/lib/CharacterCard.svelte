d2cc51907a24c9da782f585c7f3339b90e6a9f91
Egor Antonov <antonovegorv@mail.ru>
2021-09-28 15:23:36 +0300

    Created characters page

---
+++ src/lib/CharacterCard.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let character;
+</script>
+
+<a href="/characters/{character.id}">
+    <div class="p-4 h-full rounded-lg border-2 border-gray-200 bg-gray-100 hover:bg-gray-200">
+        <div>
+            <img class="w-full rounded" src="{character.images.avatar}.webp" alt="{character.name}">
+        </div>
+        <h3 class="mt-2 font-roboto">{character.name}</h3>
+        <p class="text-gray-400">{character.info}</p>
+    </div>
+</a>
---
+++ src/routes/characters/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { characters } from '../../stores/character.store.js';
+
+    import CharacterCard from '../../lib/CharacterCard.svelte';
+</script>
+
+<section>
+    <h1 class="text-4xl font-medium font-roboto">Персонажи мультфильма Тачки</h1>
+    <div class="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-10">
+        {#each $characters as character}
+            <CharacterCard character={character} />
+        {/each}
+    </div>
+</section>
